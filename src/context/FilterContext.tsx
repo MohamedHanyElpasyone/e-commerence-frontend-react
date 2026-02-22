66fde52f291efea8b3e80bc5cc867eefbdca4db4
Mohamed Hany | Front-End-Developer <mhany.developer@gmail.com>
2026-02-22 11:09:15 +0200

    Create "FilterContext", and wrap the App inside "FilterProvider"
    -Create "FilterContext" to share states between components to avoid drilling props
    -Wrap App inside "FilterProvider" to ensure accessed values to components.

---
+++ src/context/FilterContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+
+interface FilterProviderProps {
+  children: ReactNode;
+}
+
+interface FilterContextType {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+  minPrice: number | undefined;
+  setMinPrice: (price: number | undefined) => void;
+  maxPrice: number | undefined;
+  setMaxPrice: (price: number | undefined) => void;
+  selectedCategory: string;
+  setSelectedCategory: (category: string) => void;
+  keyword: string;
+  setKeyword: (Keyword: string) => void;
+}
+
+const FilterContext = createContext<FilterContextType | undefined>(undefined);
+
+export const FilterProvider = ({ children }: FilterProviderProps) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [keyword, setKeyword] = useState("");
+
+  return (
+    <FilterContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        minPrice,
+        setMinPrice,
+        maxPrice,
+        setMaxPrice,
+        selectedCategory,
+        setSelectedCategory,
+        keyword,
+        setKeyword,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilter = () => {
+  const context = useContext(FilterContext)
+
+  if (context === undefined){
+    throw new Error("useFilter must be used with a FilterProvider")
+  }
+
+  return context;
+};
